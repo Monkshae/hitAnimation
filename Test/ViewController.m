93d0670f5e8212ce947c56e4b88accbf8e6bfa2b
licong <licong@wepie.com>
2020-10-30 19:07:16 +0800

    layer改成UILabel可暂停动画

--- Test/ViewController.m
+++ Test/ViewController.m
@@ -10,8 +10,10 @@
 
 @interface ViewController ()
 
-@property (nonatomic, weak) CALayer *movingLayer;
+//@property (nonatomic, weak) CALayer *movingLayer;
+@property (nonatomic, strong) UILabel *movingLabel;
 @property (nonatomic, strong) UITapGestureRecognizer *tapGesture;
+//@property (nonatomic, strong) UILongPressGestureRecognizer *longTapGesture;
 
 @end
 
@@ -19,43 +21,60 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    [self.view.layer addSublayer:self.movingLayer];
+    [self.view addSubview:self.movingLabel];
     [self addAnimation];
     
     self.tapGesture = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(click:)];
     [self.view addGestureRecognizer:self.tapGesture];
 }
 
+- (void)addMenu {
+    UIMenuController *menu = [UIMenuController sharedMenuController];
+    menu.arrowDirection = UIMenuControllerArrowDown;
+    UIMenuItem *reportItem = [[UIMenuItem alloc] initWithTitle:@"举报" action:@selector(reportAction:)];
+    menu.menuItems = @[reportItem];
+    CGRect targetRect = CGRectMake(self.movingLabel.layer.presentationLayer.position.x, self.movingLabel.layer.presentationLayer.position.y - 50, 44, 44);
+    [menu showMenuFromView:self.view rect:targetRect];
+}
+
+- (void)reportAction:(id)sender {
+    
+}
+
 -(void)click:(UITapGestureRecognizer *)tapGesture {
     CGPoint touchPoint = [tapGesture locationInView:self.view];
-    if ([self.movingLayer.presentationLayer hitTest:touchPoint]) {
+    if ([self.movingLabel.layer.presentationLayer hitTest:touchPoint]) {
         NSLog(@"presentationLayer");
-        if (self.movingLayer.speed == 0) {
+        if (self.movingLabel.layer.speed == 0) {
             [self startAnimating];
         } else {
             [self stopAnimating];
+            [self addMenu];
+            NSLog(@"position = %@",NSStringFromCGPoint(self.movingLabel.layer.presentationLayer.position));
+
+
         }
     }
 }
 
 -(void)startAnimating {
     //先判断是否已设置动画，如果已设置则执行动画
-    if([self.movingLayer animationForKey:@"moveAnimationKey"]){
+    if([self.movingLabel.layer animationForKey:@"moveAnimationKey"]){
         //如果动画正在执行则返回，避免重复执行动画
-        if (self.movingLayer.speed == 1) {
+        if (self.movingLabel.layer.speed == 1) {
             //speed == 1表示动画正在执行
             return;
         }
         //让动画执行
-        self.movingLayer.speed = 1;
+        self.movingLabel.layer.speed = 1;
         //取消上次设置的时间
-        self.movingLayer.beginTime = 0;
+        self.movingLabel.layer.beginTime = 0;
         //获取上次动画停留的时刻
-        CFTimeInterval pauseTime = self.movingLayer.timeOffset;
+        CFTimeInterval pauseTime = self.movingLabel.layer.timeOffset;
         //取消上次记录的停留时刻
-        self.movingLayer.timeOffset = 0;
+        self.movingLabel.layer.timeOffset = 0;
         //计算暂停的时间，设置相对于父坐标系的开始时间
-        self.movingLayer.beginTime = [self.movingLayer convertTime:CACurrentMediaTime() fromLayer:nil] - pauseTime;
+        self.movingLabel.layer.beginTime = [self.movingLabel.layer convertTime:CACurrentMediaTime() fromLayer:nil] - pauseTime;
     } else{
         //添加动画
         [self addAnimation];
@@ -64,39 +83,57 @@
 -(void)addAnimation
 {
     CAKeyframeAnimation *moveLayerAnimation = [CAKeyframeAnimation animationWithKeyPath:@"position"];
-    moveLayerAnimation.values = @[[NSValue valueWithCGPoint:CGPointMake(0, 100)],
-                                  [NSValue valueWithCGPoint:CGPointMake([UIScreen mainScreen].bounds.size.width - self.movingLayer.bounds.size.width, 100)]];
+    moveLayerAnimation.values = @[[NSValue valueWithCGPoint:CGPointMake(50, 150)],
+                                  [NSValue valueWithCGPoint:CGPointMake([UIScreen mainScreen].bounds.size.width - self.movingLabel.layer.bounds.size.width/2, 150)]];
     moveLayerAnimation.duration = 2.0;
     moveLayerAnimation.autoreverses = YES;
+    moveLayerAnimation.removedOnCompletion = NO;
     moveLayerAnimation.repeatCount = INFINITY;
+    moveLayerAnimation.fillMode = kCAFillModeForwards;
     moveLayerAnimation.timingFunction = [CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionLinear];
-    [self.movingLayer addAnimation:moveLayerAnimation forKey:@"moveAnimationKey"];
+    [self.movingLabel.layer addAnimation:moveLayerAnimation forKey:@"moveAnimationKey"];
 }
 
 -(void)stopAnimating {
     //如果动画已经暂停，则返回，避免重复，时间会记录错误，造成动画继续后不能连续。
-    if (self.movingLayer.speed == 0) {
+    if (self.movingLabel.layer.speed == 0) {
         return;
     }
     //将当前动画执行到的时间保存到layer的timeOffet中
    //一定要先获取时间再暂停动画
-    CFTimeInterval pausedTime = [self.movingLayer convertTime:CACurrentMediaTime() fromLayer:nil];
+    CFTimeInterval pausedTime = [self.movingLabel.layer convertTime:CACurrentMediaTime() fromLayer:nil];
     //将动画暂停
-    self.movingLayer.speed = 0;
+    self.movingLabel.layer.speed = 0;
     //记录动画暂停时间
-    self.movingLayer.timeOffset = pausedTime;
+    self.movingLabel.layer.timeOffset = pausedTime;
 }
 
-- (CALayer *)movingLayer {
-    if (!_movingLayer) {
-        CGSize layerSize = CGSizeMake(100, 100);
-        CALayer *movingLayer = [CALayer layer];
-        movingLayer.bounds = CGRectMake(0, 100, layerSize.width, layerSize.height);
-        movingLayer.anchorPoint = CGPointMake(0, 0);
-        [movingLayer setBackgroundColor:[UIColor orangeColor].CGColor];
-        _movingLayer = movingLayer;
+- (UILabel *)movingLabel {
+    if (!_movingLabel) {
+        UILabel *movingLabel = [[UILabel alloc]initWithFrame:CGRectMake(0, 100, 100, 100)];
+        movingLabel.text = @"这是一个测试";
+        movingLabel.font = [UIFont systemFontOfSize:14];
+        movingLabel.textAlignment = NSTextAlignmentLeft;
+        movingLabel.backgroundColor = UIColor.purpleColor;
+        _movingLabel = movingLabel;
     }
-    return _movingLayer;
+    return _movingLabel;
+}
+
+- (BOOL)canBecomeFirstResponder {
+    return YES;
+}
+
+- (BOOL)becomeFirstResponder {
+    return [super becomeFirstResponder];
+}
+
+- (BOOL)canPerformAction:(SEL)action withSender:(id)sender
+{
+    if (action == @selector(reportAction:)) {
+        return YES;
+    }
+    return NO;
 }
 
 - (void)dealloc {
